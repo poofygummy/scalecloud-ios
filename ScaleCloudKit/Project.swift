1ae43193aa6c785e948bf86fc7290b4ebee71688
PoofyGummy <poofygummy@gmail.com>
2026-06-05 00:59:44 +0200

    fixing workflows

--- ScaleCloudKit/Project.swift
+++ ScaleCloudKit/Project.swift
@@ -12,7 +12,7 @@ let project = Project(
             deploymentTargets: .iOS("14.0"),
             sources: ["Sources/**"],
             dependencies: [
-                .framework(path: "../ScaleCloudGo/ScaleCloudGo.framework"),
+                .project(target: "ScaleCloudGo", path: "../ScaleCloudGo"),
                 .external(name: "Alamofire"),
                 .external(name: "SwiftyJSON"),
                 .external(name: "SwiftyXMLParser")
