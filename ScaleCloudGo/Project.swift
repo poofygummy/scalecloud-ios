2078a7b19d4eb29cd47311801ad72ec50b9bdb7b
PoofyGummy <poofygummy@gmail.com>
2026-06-05 04:44:01 +0200

    fixing workflows

--- ScaleCloudGo/Project.swift
+++ ScaleCloudGo/Project.swift
@@ -25,26 +25,6 @@ rm -rf "$TMP_XCFRAMEWORK"
 gomobile bind -target=ios/arm64 -o "$TMP_XCFRAMEWORK" "$PROJECT_DIR"
 
 cp -R "$TMP_XCFRAMEWORK/ios-arm64/ScaleCloudGo.framework" "$TARGET_FRAMEWORK"
-
-# Create Headers directory and umbrella header
-mkdir -p "$TARGET_FRAMEWORK/Headers"
-UMBRELLA="$TARGET_FRAMEWORK/Headers/ScaleCloudGo.h"
-
-# Create a minimal umbrella header (gomobile sometimes puts the header at framework root)
-if [ -f "$TARGET_FRAMEWORK/ScaleCloudGo.h" ]; then
-    cp "$TARGET_FRAMEWORK/ScaleCloudGo.h" "$UMBRELLA"
-else
-    echo '// ScaleCloudGo umbrella header' > "$UMBRELLA"
-fi
-
-# Create module map
-mkdir -p "$TARGET_FRAMEWORK/Modules"
-cat > "$TARGET_FRAMEWORK/Modules/module.modulemap" << EOF
-framework module ScaleCloudGo {
-    umbrella header "ScaleCloudGo.h"
-    export *
-}
-EOF
 """,
                     name: "Build Go Framework"
                 )
