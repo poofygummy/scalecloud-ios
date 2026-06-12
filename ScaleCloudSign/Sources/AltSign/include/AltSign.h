fa203a682f13f703175360bdc1ab03822b0b5d97
PoofyGummy <poofygummy@gmail.com>
2026-06-12 02:17:30 +0200

    Fix AltSign.h header paths to be relative to AltSign source root
    - Update AltSign.h to use paths relative to Sources/AltSign/ directory
    - Add Sources/AltSign to HEADER_SEARCH_PATHS so umbrella header can find all imports
    - This allows module.modulemap to properly build CAltSign module

--- ScaleCloudSign/Sources/AltSign/include/AltSign.h
+++ ScaleCloudSign/Sources/AltSign/include/AltSign.h
@@ -7,30 +7,30 @@
 //
 
 // Apple API
-#import "../AppleAPI/ALTAppleAPI.h"
-#import "../AppleAPI/ALTAppleAPISession.h"
+#import "AppleAPI/ALTAppleAPI.h"
+#import "AppleAPI/ALTAppleAPISession.h"
 
 // Signing
-#import "../Signing/ALTSigner.h"
+#import "Signing/ALTSigner.h"
 
 // Model
-#import "../Model/ALTApplication.h"
-#import "../Model/Apple API/ALTAccount.h"
-#import "../Model/Apple API/ALTAnisetteData.h"
-#import "../Model/Apple API/ALTTeam.h"
-#import "../Model/Apple API/ALTDevice.h"
-#import "../Model/Apple API/ALTCertificate.h"
-#import "../Model/Apple API/ALTAppID.h"
-#import "../Model/Apple API/ALTAppGroup.h"
-#import "../Model/Apple API/ALTProvisioningProfile.h"
+#import "Model/ALTApplication.h"
+#import "Model/Apple API/ALTAccount.h"
+#import "Model/Apple API/ALTAnisetteData.h"
+#import "Model/Apple API/ALTTeam.h"
+#import "Model/Apple API/ALTDevice.h"
+#import "Model/Apple API/ALTCertificate.h"
+#import "Model/Apple API/ALTAppID.h"
+#import "Model/Apple API/ALTAppGroup.h"
+#import "Model/Apple API/ALTProvisioningProfile.h"
 
 // Categories
-#import "../Categories/NSError+ALTErrors.h"
-#import "../Categories/NSFileManager+Zip.h"
-#import "../Categories/NSCharacterSet+ASCII.h"
+#import "Categories/NSError+ALTErrors.h"
+#import "Categories/NSFileManager+Zip.h"
+#import "Categories/NSCharacterSet+ASCII.h"
 
 // Capabilities
-#import "../Capabilities/ALTCapabilities.h"
+#import "Capabilities/ALTCapabilities.h"
 
 //! Project version number for AltSign.
 FOUNDATION_EXPORT double AltSignVersionNumber;
